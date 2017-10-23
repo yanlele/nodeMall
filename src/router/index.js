3df19d73ff96fbe897d12ffb8d2df68d627e69a0
yanlele <331393627@qq.com>
2017-10-23 13:58:16 +0800

    首页商品列表页组件化

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import GoodsList from '../views/GoodsList.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  mode:'history',
   routes: [
     {
-      path: '/goods/:goodsId/user/:name',
+      path: '/',
       name: 'GoodsList',
       components: {
         default:GoodsList,
-        title:Title,
-        img:Image
       }
     }
   ]
