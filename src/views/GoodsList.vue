6f8897efb7514a96eaaff9b78588581a0463eaae
yanlele <331393627@qq.com>
2017-10-23 18:23:52 +0800

    mock完成、通过index切换价格状态功能

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -10,7 +10,11 @@
         <div class="filter-nav">
           <span class="sortby">Sort by:</span>
           <a href="javascript:void(0)" class="default cur">Default</a>
-          <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+          <a href="javascript:void(0)" class="price">Price
+            <svg class="icon icon-arrow-short">
+              <use xlink:href="#icon-arrow-short"></use>
+            </svg>
+          </a>
           <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
         </div>
         <div class="accessory-result">
@@ -18,18 +22,9 @@
           <div class="filter stopPop" id="filter">
             <dl class="filter-price">
               <dt>Price:</dt>
-              <dd><a href="javascript:void(0)">All</a></dd>
-              <dd>
-                <a href="javascript:void(0)">0 - 100</a>
-              </dd>
-              <dd>
-                <a href="javascript:void(0)">100 - 500</a>
-              </dd>
-              <dd>
-                <a href="javascript:void(0)">500 - 1000</a>
-              </dd>
-              <dd>
-                <a href="javascript:void(0)">1000 - 2000</a>
+              <dd><a href="javascript:void(0)" :class="{'cur':priceChecked=='all'}" @click="priceChecked='all'">All</a></dd>
+              <dd v-for="(price,index) in priceFilter" >
+                <a href="javascript:void(0)" @click="priceChecked=index" :class="{'cur':priceChecked==index}" >{{price.startPrice}} - {{price.endPrice}}</a>
               </dd>
             </dl>
           </div>
@@ -38,49 +33,13 @@
           <div class="accessory-list-wrap">
             <div class="accessory-list col-4">
               <ul>
-                <li>
+                <li v-for="(item,index) in goodsList">
                   <div class="pic">
-                    <a href="#"><img src="/static/1.jpg" alt=""></a>
+                    <a href="#"><img :src="'/static/'+item.prodcutImg" alt=""></a>
                   </div>
                   <div class="main">
-                    <div class="name">XX</div>
-                    <div class="price">999</div>
-                    <div class="btn-area">
-                      <a href="javascript:;" class="btn btn--m">加入购物车</a>
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div class="pic">
-                    <a href="#"><img src="/static/2.jpg" alt=""></a>
-                  </div>
-                  <div class="main">
-                    <div class="name">XX</div>
-                    <div class="price">1000</div>
-                    <div class="btn-area">
-                      <a href="javascript:;" class="btn btn--m">加入购物车</a>
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div class="pic">
-                    <a href="#"><img src="/static/3.jpg" alt=""></a>
-                  </div>
-                  <div class="main">
-                    <div class="name">XX</div>
-                    <div class="price">500</div>
-                    <div class="btn-area">
-                      <a href="javascript:;" class="btn btn--m">加入购物车</a>
-                    </div>
-                  </div>
-                </li>
-                <li>
-                  <div class="pic">
-                    <a href="#"><img src="/static/4.jpg" alt=""></a>
-                  </div>
-                  <div class="main">
-                    <div class="name">XX</div>
-                    <div class="price">2499</div>
+                    <div class="name">{{item.productName}}</div>
+                    <div class="price">{{item.prodcutPrice}}</div>
                     <div class="btn-area">
                       <a href="javascript:;" class="btn btn--m">加入购物车</a>
                     </div>
@@ -106,15 +65,49 @@
   import NavBread from '@/components/NavBread'
   import axios from 'axios'
 
-
   export default{
 
     name: 'GoodsList',
 
-    components:{
+    data(){
+      return {
+        goodsList: [],
+        priceFilter: [
+          {
+            startPrice: '0.00',
+            endPrice:'500.00',
+          },
+          {
+            startPrice: '500.00',
+            endPrice:'1000.00',
+          },
+          {
+            startPrice: '1000.00',
+            endPrice:'2000.00',
+          }
+        ],
+        priceChecked:'all'
+      }
+    },
+
+    components: {
       NavHeader,
       NavFooter,
       NavBread
+    },
+
+    mounted(){
+      this.getGoodsList();
+    },
+
+
+    methods: {
+      getGoodsList(){
+        axios.get('/goods').then((res) => {
+          var res = res.data;
+          this.goodsList = res.result;
+        })
+      }
     }
   }
 </script>
