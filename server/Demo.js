1a80b2ad143262dfa48523be81cbd1852733bfc9
yanlele <331393627@qq.com>
2017-10-24 08:37:10 +0800

    对req.url和url模块，util模块的分析

--- server/Demo.js
+++ server/Demo.js
@@ -12,7 +12,11 @@ let server= http.createServer((req,res)=>{
   res.statusCode=200;
   res.setHeader('Conent-Type','text/plain; charset=utf-8');
 
-  console.log(`url: ${req.url}`);
+  console.log(`url: ${req.url}`);//得到的是域名和断后之后的url
+
+  console.log(`parse: ${url.parse(req.url)}`);//得到的是一个对象
+
+  console.log(`inspect: ${util.inspect(url.parse(req.rul))}`);//可以吧对象解析为字符串
 
   res.end(util.inspect(url.parse('http://127.0.0.1:3000/index.html?a=123#tag')))
 });
