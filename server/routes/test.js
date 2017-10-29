9cc316b6f0b0ac485b3b8a538aace490bd3fd3c8
yanlele <331393627@qq.com>
2017-10-29 09:37:50 +0800

    后端功能：写的最基本的正删改查的一些示例而已

--- server/routes/test.js
+++ server/routes/test.js
@@ -6,18 +6,18 @@ var User = require('../models/user');
 
 
 /*
-* 测试增删改查功能
-* 首先第一点，就是对应表结构的时候，一定要严格来定义
-* */
+ * 测试增删改查功能
+ * 首先第一点，就是对应表结构的时候，一定要严格来定义
+ * */
 /*
-* 第一个功能插入数据库(增加数据库):Schema实例化一个对应数据库的对象
-* 增加数据库有两个方法，一个save(),另外一个是create()方法
-* 支持内部回调，内部回调分别是err,doc，
-* 外部promise回调的参数分别是doc,len,err
-* 这两个方法的区别是，save是，如果没有，创建新的对象集合，如果有，就覆盖旧的对象集合
-* create()不管有没有旧的对象集合，都创建新的对象集合（可能会出现很多相同的对象集合）
-* 具体情况分业务场景来定！
-* */
+ * 第一个功能插入数据库(增加数据库):Schema实例化一个对应数据库的对象
+ * 增加数据库有两个方法，一个save(),另外一个是create()方法
+ * 支持内部回调，内部回调分别是err,doc，
+ * 外部promise回调的参数分别是doc,len,err
+ * 这两个方法的区别是，save是，如果没有，创建新的对象集合，如果有，就覆盖旧的对象集合
+ * create()不管有没有旧的对象集合，都创建新的对象集合（可能会出现很多相同的对象集合）
+ * 具体情况分业务场景来定！
+ * */
 let lele = mongoose.model('lele', {
   result: {
     size: Number,
@@ -29,12 +29,12 @@ let le = new lele({
   result: {
     size: 15,
     address: {
-      detail:'北京',
-      name:'yanle'
+      detail: '北京',
+      name: 'yanle'
     }
   }
 });
-router.get('/save', (req, res, next) => {
+router.get('/create', (req, res, next) => {
   //第一个参数是结果，第二个参数是返回影响数据条数，第三个参数是异常
   /*    le.save().then((doc, len,err)=>{
    console.log(`result :${doc}`);
@@ -45,13 +45,13 @@ router.get('/save', (req, res, next) => {
    });*/
 
   //第一个参数是异常，第二个参数是结果，第三个参数是印象条数
-/*  le.save((err, doc, len) => {
-    console.log(`result :${doc}`);
-    console.log(`err: ${err}`);
-    console.log(`three : ${len}`);
+  /*  le.save((err, doc, len) => {
+   console.log(`result :${doc}`);
+   console.log(`err: ${err}`);
+   console.log(`three : ${len}`);
 
-    res.status(200).json(doc);
-  })*/
+   res.status(200).json(doc);
+   })*/
 
 
   /*    lele.create({
@@ -70,17 +70,58 @@ router.get('/save', (req, res, next) => {
     result: {
       size: 16,
       address: {
-        detail:'重庆'
+        detail: '重庆'
       },
-      name:'yanle'
+      name: 'yanle'
     }
-  },(err, doc,len)=>{
+  }, (err, doc, len) => {
     console.log(`result :${doc}`);
     console.log(`err: ${err}`);
     console.log(`three : ${len}`);
     res.status(200).json(doc);
   })
 });
+
+/*修改
+ *
+ * */
+//这种办法实际上只能修改一个数据!  updateMany就可以批量修改了
+router.get('/update', (req, res, next) => {
+  lele.updateMany({
+    'result.size':16
+    }, {
+    $set: {
+      'result.address': '不告诉你！！！！'
+    }}, (err, doc) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(err);
+        res.status(200).json(doc)
+      }
+    })
+});
+
+/*查询*/
+router.get('/find',function(req,res,next){
+  lele.find({
+    'result.size':16
+  },(err,data)=>{
+    res.status(200).json(data);
+  })
+});
+
+/*删除操作*/
+router.get('/delete',(req,res,next)=>{
+  lele.remove({
+    'result.size':16
+  },(err,doc)=>{
+    console.log(err);
+    console.log(doc);
+    res.status(200).json(doc)
+  })
+});
+
 
 
 /**
